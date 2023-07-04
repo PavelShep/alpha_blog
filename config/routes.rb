31de5540b0baf74dd13ac9cd1cd4269b2946f443
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-07-04 12:14:11 +0200

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
+
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles 
   get "signup", to: "users#new"
   resources :users, except: [:new]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
