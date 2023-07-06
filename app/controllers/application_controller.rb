ec9a5ef5d9a5bdf8b4bddcd6f5ee9a6facba0bf9
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-07-06 11:23:54 +0200

    change helper methods to controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  # to be visible also in the views
+  helper_method :current_user, :logged_in?
+
+  # to be visible in the controllers
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user # convert to bool
+  end
+
 end
