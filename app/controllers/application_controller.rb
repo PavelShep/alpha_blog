5a6379cc1085f0eab692e5b7b941bd497d6e88c2
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-07-07 13:40:35 +0200

    add restrictions in url to articles_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,10 @@ class ApplicationController < ActionController::Base
     !!current_user # convert to bool
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
