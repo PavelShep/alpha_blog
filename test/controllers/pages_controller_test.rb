55443d4f4a64cbd310e5d790bf5e58b483227c6f
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-07-16 15:11:43 +0200

    add tests for pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
+
+  test "should get home(root) if not logged_in" do
+    get root_url
+    assert_response :success
+  end
+
+  test "get articles if logged_in" do
+    @user = User.create(username: "johndoe", email: "johndoe@example.com", password: "password", admin: false)
+    sign_in_as(@user)
+    get root_url
+    assert_redirected_to articles_url
+  end
+
 end
