1fc78aae1fc2a9f6e4ec44211dac39cf5fafce6b
Litwin <71721882+PavelShep@users.noreply.github.com>
2023-08-09 18:06:38 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Create simple Users
+User.create!(:username => 'John Doe', :email => 'john@gmail.com', :password => 'password', :password_confirmation => 'password', :admin => false)
+User.create!(:username => 'John Morgan', :email => 'morgan@gmail.com', :password => 'password1', :password_confirmation => 'password1', :admin => false)
+# Create admin Users
+User.create!(:username => 'Alex Smith', :email => 'alexsmith@gmail.com', :password => 'password12', :password_confirmation => 'password12', :admin => true)
+
+# Create Categories
+Category.create!([{
+  name: "Sports"
+},
+{
+  name: "Biology"
+},
+{
+ name: "English"
+}])
+
+# Create Articles
+Article.create!(title: "Swimming", description: "Considered an invitation do introduced sufficient understood instrument it.", user_id: 1)
+Article.create!(title: "Tigers", description: "Considered an invitation do introduced sufficient understood instrument it.", user_id: 2)
+
+# Create Article_Categories
+ArticleCategory.create!(article_id: "1", category_id: "1")
+ArticleCategory.create!(article_id: "2", category_id: "2")
+
